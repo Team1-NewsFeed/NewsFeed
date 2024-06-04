76a4090f24113267d70498381aadbf7efe3c6c79
Imnotcoderdude <topsolo7@naver.com>
2024-06-04 20:20:08 +0900

    add : 한정운님 Timer Entity class 생성

---
+++ src/main/java/com/sparta/newsfeedproject/entity/Timer.java
@@ -0,0 +1,23 @@
+package com.sparta.newsfeedproject.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timer {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime created_time;
+
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifie_time;
+}
