1714eb6f37bc0a468e6237808f0b2a828af3a537
jiwoo <1114jiwoo@gmail.com>
2024-06-05 16:26:47 +0900

    [update] 프로필 조회, 프로필 수정 (인증X)

--- src/main/java/com/sparta/newsfeed/service/UserService.java
+++ src/main/java/com/sparta/newsfeed/service/UserService.java
@@ -5,7 +5,6 @@ import com.sparta.newsfeed.dto.UserRequest;
 import com.sparta.newsfeed.dto.UserResponse;
 import com.sparta.newsfeed.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
