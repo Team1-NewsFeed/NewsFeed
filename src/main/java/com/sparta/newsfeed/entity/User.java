fc8722b8c5b1f6ad89cd2a6c62a5429dc848eb64
Imnotcoderdude <topsolo7@naver.com>
2024-06-07 00:44:14 +0900

    add : User 엔티티 클래스와 그에 맞는 Repository 인터페이스 새로 생성.

--- src/main/java/com/sparta/newsfeed/entity/User.java
+++ src/main/java/com/sparta/newsfeed/entity/User.java
@@ -1,20 +1,58 @@
 package com.sparta.newsfeed.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String User_id;
+    @Column
+    private String userId;
 
+    @Column
+    @NotBlank(message = "유저이름은 비워둘 수 없습니다.")
+    private String username;
 
+    @Column
+    @NotBlank(message = "비밀번호는 비워둘 수 없습니다.")
+    private String password;
+
+    @Column
+    @NotBlank(message = "이메일은 비워둘 수 없습니다.")
+    private String email;
+
+    @Column
+    @NotBlank(message = "한줄 소개는 비워둘 수 없습니다.")
+    private String one_liner;
+
+    @Column
     private String user_status;
+
+    @Column
     private String refresh_token;
 
+    public void setUser_id(String userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(@NotBlank(message = "유저이름은 비워둘 수 없습니다.") String username) {
+        this.username = username;
+    }
+
+    public void setPassword(@NotBlank(message = "비밀번호는 비워둘 수 없습니다.") String password) {
+        this.password = password;
+    }
+
+    public void setEmail(@NotBlank(message = "이메일은 비워둘 수 없습니다.") String email) {
+        this.email = email;
+    }
+
+    public void setOne_liner(@NotBlank(message = "한줄 소개는 비워둘 수 없습니다.") String one_liner) {
+        this.one_liner = one_liner;
+    }
 }
