ff64bed12957662b7ef6aca652e409867ce6b86d
Imnotcoderdude <topsolo7@naver.com>
2024-06-07 00:45:05 +0900

    delete : 이전 엔티티 클래스인 SignUp 엔티티 클래스와 레파지토리 인터페이스 삭제

--- src/main/java/com/sparta/newsfeed/repository/SignUpRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.newsfeed.repository;
-
-import com.sparta.newsfeed.entity.Signup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SignUpRepository extends JpaRepository<Signup, Long> {
-    Signup findByUser_id(String userId);
-}
