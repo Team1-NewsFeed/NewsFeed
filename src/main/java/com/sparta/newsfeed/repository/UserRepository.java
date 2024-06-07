d4d0a783637af13f9e4ad8f50ba68fd3ae02b4b0
jiwoo <1114jiwoo@gmail.com>
2024-06-07 15:56:37 +0900

    [update] 프로필 조회, 프로필 수정 (인증X)

--- src/main/java/com/sparta/newsfeed/repository/UserRepository.java
+++ src/main/java/com/sparta/newsfeed/repository/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sparta.newsfeed.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+//    User findByUsername(String username);
 }
